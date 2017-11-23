6fd5f06c641ab59264df1cc9e26961f83621ac25
hiro0218 <hiro0218@gmail.com>
2017-11-24 01:38:38 +0900

    fix: css to scoped

--- components/MenuNavi.vue
+++ components/MenuNavi.vue
@@ -12,54 +12,54 @@
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .menu-container {
   margin-top: 57px;
+}
 
-  ul {
-    list-style-type: none;
-    margin: 0 0 0 23px;
-    padding: 0;
+ul {
+  list-style-type: none;
+  margin: 0 0 0 23px;
+  padding: 0;
+}
+
+li {
+  white-space: nowrap;
+  & + li {
+    margin-top: 33px;
   }
 
-  li {
-    white-space: nowrap;
-    & + li {
-      margin-top: 33px;
-    }
+  &::before {
+    content: "●";
+    margin-right: 4px;
+  }
 
-    &::before {
-      content: "●";
-      margin-right: 4px;
-    }
+  &.top::before {
+    color: #FFCCCC;
+  }
 
-    &.top::before {
-      color: #FFCCCC;
-    }
+  &.movie::before {
+    color: #00FF00;
+  }
 
-    &.movie::before {
-      color: #00FF00;
-    }
+  &.tv::before {
+    color: #33FFFF;
+  }
 
-    &.tv::before {
-      color: #33FFFF;
-    }
+  &.stage::before {
+    color: #0099FF;
+  }
 
-    &.stage::before {
-      color: #0099FF;
-    }
+  &.photo::before {
+    color: #0000FF;
+  }
 
-    &.photo::before {
-      color: #0000FF;
-    }
+  &.essay::before {
+    color: #333399;
+  }
 
-    &.essay::before {
-      color: #333399;
-    }
-
-    &.kanri::before {
-      color: #CC00CC;
-    }
+  &.kanri::before {
+    color: #CC00CC;
   }
 }
 </style>
